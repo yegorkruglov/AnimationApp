7158a28b7f4de13c340c51fa81c31e6f8e4b5b8f
Egor Kruglov <yegorkruglov@gmail.com>
2023-06-13 12:26:16 +0300

    gitignorм, mvc folders, model, datastore

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,39 @@
+//
+//  Animation.swift
+//  SpringAnimation
+//
+//  Created by Egor Kruglov on 13.06.2023.
+//
+
+struct Animation {
+    let name: String
+    let curve: String
+    let force: Double
+    let duration: Double
+    let delay: Double
+    
+    static func getAnimations() -> [Animation] {
+        var animations: [Animation] = []
+        
+        var animationsNames = DataStore.shared.animationsNames.shuffled()
+        let curves = DataStore.shared.curves.shuffled()
+        
+        for animationsName in animationsNames {
+            let animation = animationsName
+            let curve = curves.randomElement() ?? "easeIn"
+            let force = Double.random(in: 0.5...3)
+            let duaration = Double.random(in: 0.5...3)
+            let delay = Double.random(in: 0...1)
+            
+            animations.append(Animation(
+                name: animation,
+                curve: curve,
+                force: force,
+                duration: duaration,
+                delay: delay))
+        }
+        
+        return animations
+    }
+    
+}
---
+++ AnimationApp/Services/DataStore.swift
@@ -0,0 +1,74 @@
+//
+//  DataStore.swift
+//  SpringAnimation
+//
+//  Created by Egor Kruglov on 13.06.2023.
+//
+
+final class DataStore {
+    static let shared = DataStore()
+    
+    let animationsNames = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let curves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    private init() {}
+}
